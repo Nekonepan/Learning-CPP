92adb1005a61017538ffbfbf657479e8edb3227c
Nekonepan <lutfanalaudin@gmail.com>
2026-04-30 18:28:50 +0700

    Innitial commit

--- Algoritms-Strategy/Greedy-algorithm/post-improvement.cpp
+++ Algoritms-Strategy/Greedy-algorithm/post-improvement.cpp
@@ -4,27 +4,26 @@ using namespace std;
 
 int main() {
     int A;
-    int B;
 
     cout << "=====================================\n";
     cout << " PROGRAM PENUKARAN UANG (GREEDY)\n";
     cout << "=====================================\n\n";
 
     // Default pecahan Rupiah
-    int C[100];
-    int n = 6;
-
-    cout << "Masukkan jumlah pecahan uang : ";
-    cin >> B;
+    int n;
     
-    for (int i = 0; i < B; i++) {
-    	cout << "Masukkan coin ke-" << i + 1 << ": ";
-    	cin >> C[i];
+    cout << "Masukkan jumlah pecahan uang : ";
+    cin >> n;
+    int C[n];
+    
+    for (int i = 0; i < n; i++) {
+        cout << "Masukkan pecahan uang ke-" << i + 1 << ": ";
+        cin >> C[i];
 	}
 	
 	// SORTING
-	for (int i = 0; i < B; i++) {
-		for (int j = i + 1; j < B; j++ ) {
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++ ) {
 			if (C[i] < C[j]) {
 				swap(C[i], C[j]);
 			}
@@ -35,6 +34,7 @@ int main() {
     cin >> A;
 
     int total = 0;
+    int jumlah_uang_pecahan = 0;
 
     cout << "\n=====================================\n";
     cout << "        HASIL PENUKARAN UANG\n";
@@ -50,16 +50,17 @@ int main() {
         while (total + C[i] <= A) {
             total += C[i];
             jumlah++;
+            jumlah_uang_pecahan++;
         }
 
         if (jumlah > 0) {
-            cout << "Rp" << setw(10) << C[i]
-                 << " : " << jumlah << " lembar\n";
+            cout << "Rp" << setw(22) << C[i] << " : " << jumlah << " lembar\n";
         }
     }
 
     cout << "-------------------------------------\n";
     cout << left << setw(25) << "Total Ditukar" << ": Rp " << total << endl;
+    cout << left << setw(25) << "Jumlah uang pecahan" << ": " << jumlah_uang_pecahan << " Lembar" << endl;
 
     if (total != A) {
         cout << "Catatan: Tidak bisa ditukar secara tepat\n";
